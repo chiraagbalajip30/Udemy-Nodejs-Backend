dead2b32b6aac8ec5fbbdb2eea27ae073f0a3f58
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-02-03 12:47:42 +0530

    Inital node.js commit

---
+++ 01-MODULEs/index.js
@@ -0,0 +1,5 @@
+// MODULES - It is a collection of code, that provides your functionality. There are 3 types:
+
+// 1. Built In Modules
+// 2. Third Party or External Modules - which can be installed using npm intall
+// 3. Custom Modules (my own)
---
+++ Hello-World/script.js
@@ -0,0 +1,23 @@
+console.log("Hello World from script.js file");
+
+const a = 2;
+const b = 5;
+const c = 7;
+const d = 9;
+const e = 11;
+
+const sum = a + b + c + d + e;
+console.log(`Sum is ${sum}`);
+
+// alert("wejhvfkdh");  won't work
+
+// if i try writing alert, it won't work because it is browser's object not Js's.
+
+// 2 diff env to run JS
+
+// Browser (JS, Browser API - fetch, alert)
+// setTimeout - webAPI valid in browser
+
+// NodeJS - (JS, Extra - file system, file handling, etc)
+// setTimeout - valid in node.js. the reason is developer of nodejs has carefully added the things required for the backend but not everything.
+
