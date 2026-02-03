b6f0be1275ec796d41c9b0dd724dc57e8d471457
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-02-03 14:51:05 +0530

    ongoing video-21 commit

--- 01-MODULEs/index.js
+++ 01-MODULEs/index.js
@@ -1,8 +1,31 @@
-const fs = require("fs");
-// console.log(fs);
+// const fs = require("fs");
+// // console.log(fs);
 
-const content = fs.readFileSync("notes.txt", 'utf-8');
+// const content = fs.readFileSync("notes.txt", 'utf-8');
 
+// console.log(content);
 
+// ---------------------------------------------
+
+// Video -21
+// ----------
+
+const fs = require("node:fs");
+
+// read a file
+const content = fs.readFileSync("notes.txt", "utf-8");
+
+//write a file
+fs.writeFileSync(
+  "copy.txt",
+  "I am creating a new file using writeFileSync",
+  "utf-8",
+);
+
+fs.appendFileSync(
+  "copy.txt",
+  "I am creating a new file using appendFileSync and trying to add content to it",
+  "utf-8",
+);
 
 console.log(content);
