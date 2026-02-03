21cb39873fd8e25f54067070fa539bbdbac2d165
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-02-03 13:54:32 +0530

    Till Video-19 Commit

--- 01-MODULEs/index.js
+++ 01-MODULEs/index.js
@@ -1,5 +1,8 @@
-// MODULES - It is a collection of code, that provides your functionality. There are 3 types:
+const fs = require("fs");
+// console.log(fs);
 
-// 1. Built In Modules
-// 2. Third Party or External Modules - which can be installed using npm intall
-// 3. Custom Modules (my own)
+const content = fs.readFileSync("notes.txt", 'utf-8');
+
+
+
+console.log(content);
