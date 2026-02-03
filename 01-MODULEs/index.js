2c53cbdeeed3fe2af73d6f80f81fce426e7055c6
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-02-03 15:13:04 +0530

    Till Video-21 Commit

--- 01-MODULEs/index.js
+++ 01-MODULEs/index.js
@@ -16,16 +16,26 @@ const fs = require("node:fs");
 const content = fs.readFileSync("notes.txt", "utf-8");
 
 //write a file
-fs.writeFileSync(
-  "copy.txt",
-  "I am creating a new file using writeFileSync",
-  "utf-8",
-);
+// fs.writeFileSync(
+//   "copy.txt",
+//   "I am creating a new file using writeFileSync",
+//   "utf-8",
+// );
 
-fs.appendFileSync(
-  "copy.txt",
-  "I am creating a new file using appendFileSync and trying to add content to it",
-  "utf-8",
-);
+// fs.appendFileSync(
+//   "copy.txt",
+//   "\n\nI am creating a new file using appendFileSync and trying to add content to it.",
+//   "utf-8",
+// );
+
+// fs.mkdirSync('games/xyz/a', { recursive : true });
+
+// fs.rmdirSync("games/xyz/a", { recursive: true });
+
+// now we will create and delete a file
+// fs.writeFileSync("sample.txt", "I am creating a new file using writeFileSync", "utf-8");
+
+// fs.unlinkSync("sample.txt");
+
 
 console.log(content);
