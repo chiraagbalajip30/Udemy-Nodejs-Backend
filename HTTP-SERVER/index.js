b3c531c7c0bffbdf166ee34c327b020fdbe12ca8
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-02-06 15:43:14 +0530

    Video-33 Custom Server Commit

--- HTTP-SERVER/index.js
+++ HTTP-SERVER/index.js
@@ -19,22 +19,45 @@
 // });
 
 // ----------------------------------------------
-// Video-32
-// Deep dive into HTTP Request Handling and Server Routing
+// Video-33
+// Building a Custom Server - Hands on
 
 const http = require("node:http");
 
-const server = http.createServer(function (req, res) {
-  console.log("I got an incoming request");
-  // insteasd of console, you can do db operastins, etc.
-  res.writeHead(200);
-  res.end("Thanks for visitng my server");
+const server = http.createServer((req, res) => {
+  console.log("Incoming Request at ${Datew.now()}");
+  console.log(req.headers);
+  console.log(req.method);
+  console.log(req.url);
+
+  // send the response
+  //   res.writeHead(200);
+
+  // this is how you write switch cases in node.js for routes
+  switch (req.url) {
+    case "/":
+      res.writeHead(200);
+      return res.end("Home Page");
+
+    case "/contact-us":
+      res.writeHead(200);
+      return res.end("Contact Us Page");
+
+    case "/about-us":
+      res.writeHead(200);
+      return res.end("About Us Page");
+
+    default:
+      res.writeHead(400);
+      return res.end("Page Not Found");
+  }
+
+  //   res.end("OK!");
+  res.end(
+    `Hey, you can accept or view the encofdin type from the heasders ${req.headers["accept-language"]}`,
+  );
 });
 
-// // server.listen(8000, function() {
-// //     console.log('Listening on port 8000');
-// // }) or we can use arrow functions
-
 server.listen(8000, () => {
-  console.log("Listening on port 8000");
+  console.log("Server is running on port 8000");
 });
