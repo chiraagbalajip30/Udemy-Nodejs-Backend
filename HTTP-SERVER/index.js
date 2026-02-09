45636f4da2467e914b076d3469ff6bca69d86750
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-02-09 13:10:45 +0530

    Video-35 Tasks Commit

--- HTTP-SERVER/index.js
+++ HTTP-SERVER/index.js
@@ -22,42 +22,90 @@
 // Video-33
 // Building a Custom Server - Hands on
 
+// const http = require("node:http");
+
+// const server = http.createServer((req, res) => {
+//   console.log("Incoming Request at ${Datew.now()}");
+//   console.log(req.headers);
+//   console.log(req.method);
+//   console.log(req.url);
+
+//   // send the response
+//   //   res.writeHead(200);
+
+//   // this is how you write switch cases in node.js for routes
+//   switch (req.url) {
+//     case "/":
+//       res.writeHead(200);
+//       return res.end("Home Page");
+
+//     case "/contact-us":
+//       res.writeHead(200);
+//       return res.end("Contact Us Page");
+
+//     case "/about-us":
+//       res.writeHead(200);
+//       return res.end("About Us Page");
+
+//     default:
+//       res.writeHead(400);
+//       return res.end("Page Not Found");
+//   }
+
+//   //   res.end("OK!");
+//   res.end(
+//     `Hey, you can accept or view the encoding type from the headers ${req.headers["accept-language"]}`,
+//   );
+// });
+
+// server.listen(8000, () => {
+//   console.log("Server is running on port 8000");
+// });
+
+// --------------------------------------------------
+
+// Video - 34
+// -------------
+// Build a custom task with native http server using Node.js
+
 const http = require("node:http");
+const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  console.log("Incoming Request at ${Datew.now()}");
-  console.log(req.headers);
-  console.log(req.method);
-  console.log(req.url);
+  const method = req.method;
+  const path = req.url;
 
-  // send the response
-  //   res.writeHead(200);
+  const log = `\n[${Date.now()}]: ${method} ${path}`;
+  fs.appendFileSync("log.txt", log, "utf-8");
 
-  // this is how you write switch cases in node.js for routes
-  switch (req.url) {
-    case "/":
-      res.writeHead(200);
-      return res.end("Home Page");
+  switch (method) {
+    case "GET":
+      {
+        switch (path) {
+          case "/":
+            return res.writeHead(200).end("Hello from the Server"); // Task-1
 
-    case "/contact-us":
-      res.writeHead(200);
-      return res.end("Contact Us Page");
+          case "/contact-us":
+            return res
+              .writeHead(200)
+              .end(
+                "Send your email to mcb@gmail.com and contact to 9611614859",
+              ); // Task-2
 
-    case "/about-us":
-      res.writeHead(200);
-      return res.end("About Us Page");
-
-    default:
-      res.writeHead(400);
-      return res.end("Page Not Found");
+          case "/tweet":
+            return res.writeHead(200).end("Tweet-1\nTweet-2"); // Task-4
+        }
+      }
+      break;
+    case "POST":
+      switch (path) {
+        case "/tweet":
+          return res.writeHead(201).end("Your tweet was created!"); // Task-3
+      }
   }
-
-  //   res.end("OK!");
-  res.end(
-    `Hey, you can accept or view the encofdin type from the heasders ${req.headers["accept-language"]}`,
-  );
+  return res.writeHead(404).end("You're lost man!");
 });
 
 server.listen(8000, () => {
-  console.log("Server is running on port 8000");
+  console.log("HTTP Server is running on PORT 8000");
 });
