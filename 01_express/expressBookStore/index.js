1058ffb52cd0dffed3288971859985491d4099d3
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-02-10 17:43:44 +0530

    Video-41 Commit  Completed Bok Store APIs

--- 01_express/expressBookStore/index.js
+++ 01_express/expressBookStore/index.js
@@ -9,12 +9,15 @@ const books = [
   { id: 2, title: "Book Two", author: "Author Two" },
 ];
 
+// Middlewares(Plugins)
+app.use(express.json());
+// so now we have the body
+
 // Routes
 app.get("/books", (req, res) => {
   // res.setHeader('x-bd', 'mcb'); // manually fixing the header
   res.json(books);
 });
-
 
 app.get("/books/:id", (req, res) => {
   const id = parseInt(req.params.id);
@@ -31,6 +34,41 @@ app.get("/books/:id", (req, res) => {
   }
 });
 
+app.post("/books", (req, res) => {
+  const { title, author } = req.body;
+
+  if (!title || title === "") {
+    return res.status(400).json({ error: `Title is required` });
+  }
+
+  if (!author || author === "") {
+    return res.status(400).json({ error: `Author is required` });
+  }
+
+  const id = books.length + 1;
+
+  const book = { id, title, author };
+  books.push(book);
+
+  return res.status(201).json({ message: `Book created success: ${id}` });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    return res.status(400).json({ error: `id must be of type number` });
+  }
+
+  const indexToDelete = books.findIndex((e) => e.id === id);
+
+  if (indexToDelete < 0) {
+    return res.status(400).json({ error: `Book with id ${id} does not exist` });
+  } else {
+    books.splice(indexToDelete, 1);
+    return res.status(200).json({ message: `Book Deleted` });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Your server is running on port: ${PORT}`);
