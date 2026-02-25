10d301b1163559af0da8ff07ceb52783852700c7
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-02-25 17:14:22 +0530

    Video - 58  Implementing Session Based Authentication in Node.js Applications

---
+++ 01_Authentication/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 8000;
+
+app.use(express.json());
+
+const DIARY = {};
+const EMAILS = new Set();
+
+// Hey, Here is my car - Please park it and give me back a token
+// Email => Unique Car Number
+
+app.post("/signup", (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (EMAILS.has(email)) {
+    return res.status(400).json({ error: "Email already taken" });
+  }
+  // Create a Token for User
+  const token = `${Date.now()}`;
+
+  // Do a entry in Diary
+  DIARY[token] = { name, email, password };
+  EMAILS.add(email);
+
+  // user must come back with this tokeneverytime
+  return res.json({ status: "success", token });
+});
+
+app.post("/me", (req, res) => {
+  const { token } = req.body;
+  if (!token) {
+    return res.status(400).json({ error: "Missing Token" });
+  }
+
+  if (!(token in DIARY)) {
+    return res.status(400).json({ error: "Invalid Token" });
+  }
+
+  const entry = DIARY[token];
+
+  return res.json({ data: entry });
+});
+
+app.post("/private-data", )
+
+app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
