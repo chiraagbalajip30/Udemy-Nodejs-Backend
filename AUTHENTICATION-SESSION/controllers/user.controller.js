2b3b941d61f61431005f35e55402e8f4caefbd7b
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-03-03 18:28:06 +0530

    Video-59  Addong Session-Based Authentication Part-1 Commit

--- AUTHENTICATION-SESSION/controllers/user.controller.js
+++ AUTHENTICATION-SESSION/controllers/user.controller.js
@@ -1,5 +1,6 @@
+import { error } from "node:console";
 import db from "../db/index.js";
-import { usersTable } from "../db/schema.js";
+import { usersTable, userSessions } from "../db/schema.js";
 import { eq } from "drizzle-orm";
 import { randomBytes, createHmac } from "node:crypto";
 
@@ -46,7 +47,43 @@ export const signUp = async (req, res) => {
   }
 };
 
-export const logIn = async (req, res) => {};
+export const logIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  const [existingUser] = await db
+    .select({
+      id: usersTable.id,
+      email: usersTable.email,
+      salt: usersTable.salt,
+      password: usersTable.password,
+    })
+    .from(usersTable)
+    .where((table) => eq(table.email, email));
+
+  if (!existingUser) {
+    return res
+      .status(404)
+      .json({ error: `User with email ${email} doesnot exists` });
+  }
+
+  const salt = existingUser.salt;
+  const existingHash = existingUser.password;
+
+  const newHash = createHmac("sha256", salt).update(password).digest("hex");
+
+  if (newHash !== existingHash) {
+    return res.status(400).json({ error: "Incorrect Password " });
+  }
+
+  // Generate a session for user
+  const [session] = await db
+    .insert(userSessions)
+    .values({
+      userId: existingUser.id,
+    })
+    .returning({ id: userSessions.id });
+  return res.json({ status: "success", sessionId: session.id }); //this can be called as token. remember the example and relate
+};
 
 export const currentPage = async (req, res) => {};
 
