1f9d7a15b571bb2e213a382bbbd4da3614d0915e
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-03-10 18:02:13 +0530

    Video-66  Fist Commit

--- NODE-MONGO/connection.js
+++ NODE-MONGO/connection.js
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
+import dns from "node:dns";
+
+// Force Node to use reliable DNS servers
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 export const connectMongoDB = async (connectionURL) => {
   const connection = await mongoose.connect(connectionURL);
+
+  console.log("Connected DB:");
+
   return connection;
 };
