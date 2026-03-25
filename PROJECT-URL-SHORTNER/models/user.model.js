e417f1cfa33877c91317f148e5abd51ff3fcb4b7
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-03-25 15:03:24 +0530

    Video-74  Drizzle  Setup

---
+++ PROJECT-URL-SHORTNER/models/user.model.js
@@ -0,0 +1,6 @@
+import { integer, pgTable, varchar, uuid } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: uuid().primaryKey().defaultRandom(),
+  firstName: varchar('first_name', {length: 55}).notNull()
+});
