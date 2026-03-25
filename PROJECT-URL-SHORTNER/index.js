242fc9740bf9e00747b15802569e7c8de12e728e
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-03-25 15:59:50 +0530

    Video-76  Creating a Secure Signup Route

--- PROJECT-URL-SHORTNER/index.js
+++ PROJECT-URL-SHORTNER/index.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = process.env.PORT ?? 8000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   return res.json({ status: "Server is up and running.." });
 });
