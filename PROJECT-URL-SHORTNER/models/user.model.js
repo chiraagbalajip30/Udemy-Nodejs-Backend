4978b2ccfeaa277ebc503d53e5156c3b380e324f
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-03-25 15:07:57 +0530

    Video-74 unfinished commit

--- PROJECT-URL-SHORTNER/models/user.model.js
+++ PROJECT-URL-SHORTNER/models/user.model.js
@@ -2,5 +2,6 @@ import { integer, pgTable, varchar, uuid } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
-  firstName: varchar('first_name', {length: 55}).notNull()
+  firstName: varchar("first_name", { length: 55 }).notNull(),
+  lastName: varchar("last_name", { length: 55 }),
 });
