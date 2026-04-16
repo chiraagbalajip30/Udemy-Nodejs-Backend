b03ed047b0618c9f21a5b1ab5828c8c419de91d2
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-04-16 14:07:46 +0530

    Completed the Project till User routes, me route and login and signup

---
+++ PROJECT-NOTES-APP/index.js
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import express from "express";
+import userRouter from "./routes/user.routes.js";
+import { authenticationMiddleware } from "./middlewares/auth.middleware.js";
+
+const app = express();
+
+const PORT = process.env.PORT ?? 8000;
+
+// Middleware
+app.use(express.json());
+app.use(authenticationMiddleware);
+
+// Routes
+app.get("/", (req, res) => {
+  return res.json({ status: "Server is up and running.." });
+});
+
+app.use("/user", userRouter);
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
