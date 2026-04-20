8df86f7751a01bb2d8554da0a16e7f8bc11bed88
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-04-20 17:17:04 +0530

    Added CORS in the backend for connection and security purposes

--- PROJECT-NOTES-APP/index.js
+++ PROJECT-NOTES-APP/index.js
@@ -3,11 +3,18 @@ import express from "express";
 import userRouter from "./routes/user.routes.js";
 import notesRouter from "./routes/notes.routes.js";
 import { authenticationMiddleware } from "./middlewares/auth.middleware.js";
+import cors from "cors";
 
 const app = express();
 
 const PORT = process.env.PORT ?? 8000;
 
+// CORS (must be before routes)
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 // Middleware
 app.use(express.json());
 app.use(authenticationMiddleware);
