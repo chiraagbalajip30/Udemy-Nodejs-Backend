76fee3ceb3ebe6c6e2ebd04783e6a8efe1fe8535
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-04-28 12:46:56 +0530

    View Notes COmmit

--- PROJECT-NOTES-APP/frontend/src/pages/Dashboard.jsx
+++ PROJECT-NOTES-APP/frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import CreateNote from "../components/CreateNote";
 import NoteCard from "../components/NoteCard";
 
 export default function Dashboard() {
   const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        const res = await fetch("http://localhost:8000/notes", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+          console.error(data.error);
+          return;
+        }
+
+        setNotes(data.data); // 🔥 store notes
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchNotes();
+  }, []);
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-950 via-slate-900 to-slate-950 text-slate-100">
